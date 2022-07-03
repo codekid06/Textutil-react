d4437983bdf425bda63d15767cf3e8e8805f109b
codekid06 <bishtyash689@gmail.com>
2022-07-03 14:56:54 +0530

    Updates

---
+++ precache-manifest.78af57d33662db741e931f6141f5668a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0f496c28f91672529d1",
+    "url": "/Textutil-react/static/css/main.24cd288c.chunk.css"
+  },
+  {
+    "revision": "f0f496c28f91672529d1",
+    "url": "/Textutil-react/static/js/main.f0f496c2.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/Textutil-react/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "fc6bf2a49d77b21506bb",
+    "url": "/Textutil-react/static/js/2.fc6bf2a4.chunk.js"
+  },
+  {
+    "revision": "655ecbce843b8509abbb",
+    "url": "/Textutil-react/static/js/runtime~main.655ecbce.js"
+  },
+  {
+    "revision": "3434ef102c18474bb45df1f4dcb127e4",
+    "url": "/Textutil-react/index.html"
+  }
+];
